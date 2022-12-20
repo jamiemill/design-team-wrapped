a66630a6926e4416b129953f8515e563bd53e40d
Jamie Mill <jamiermill@gmail.com>
2022-12-20 13:47:54 +0000

    Add source code and disclaimer

--- layouts/layout.tsx
+++ layouts/layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react";
 import HeadTag from "../components/headtag";
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
@@ -9,8 +9,10 @@ export default function Layout({ children }: { children: ReactNode }) {
             <main className='text-neutral-700'>
                 {children}
             </main>
-            <div className='mt-8 p-8 text-neutral-400 text-center text-xs border-t'>
-                Made by <a className='underline' href="https://twitter.com/jamiemill">@jamiemill</a>. &nbsp;&nbsp;&nbsp; P.S. I'm looking for design leadership work in early-stage companies. <a href="https://jamiemill.com" className='underline'>Hire me!</a> ♥️
+            <div className='mt-8 py-8 text-neutral-400 text-center text-xs border-t'>
+                <p className="mb-4">This is an experiment by <Link className='underline' href="https://twitter.com/jamiemill">@jamiemill</Link> to try out Next.js,
+                    Vercel and TailwindCSS. There might be bugs! <Link href="https://github.com/jamiemill/design-team-wrapped" className="underline">Get the code on GitHub</Link>.</p>
+                <p>P.S. I'm looking for design leadership work in early-stage companies. <Link href="https://jamiemill.com" className='underline'>Hire me!</Link> ♥️</p>
             </div>
         </>
     )
