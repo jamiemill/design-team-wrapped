984d5e047daeab22f1f49836b181f38b8478107d
Jamie Mill <jamiermill@gmail.com>
2022-12-20 14:35:56 +0000

    Footer tweak

--- layouts/layout.tsx
+++ layouts/layout.tsx
@@ -10,8 +10,7 @@ export default function Layout({ children }: { children: ReactNode }) {
                 {children}
             </main>
             <div className='mt-8 py-8 text-neutral-400 text-center text-xs border-t'>
-                <p className="mb-4">This is an experiment by <Link className='underline' href="https://twitter.com/jamiemill">@jamiemill</Link> to try out Next.js,
-                    Vercel and TailwindCSS. There might be bugs! <Link href="https://github.com/jamiemill/design-team-wrapped" className="underline">Get the code on GitHub</Link>.</p>
+                <p className="mb-2">This is an experiment by <Link className='underline' href="https://twitter.com/jamiemill">@jamiemill</Link>. There might be bugs. <Link href="https://github.com/jamiemill/design-team-wrapped" className="underline">Get the code on GitHub</Link>.</p>
                 <p>P.S. I'm looking for design leadership work in early-stage companies. <Link href="https://jamiemill.com" className='underline'>Hire me!</Link> ♥️</p>
             </div>
         </>
